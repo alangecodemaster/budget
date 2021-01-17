4861fa0323cd8b2e7d5a3c32f453acfa07064c28
alangecodemaster <35128509+alangecodemaster@users.noreply.github.com>
2021-01-16 23:15:46 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const budgetingApp = "budgeting-app-v1"
+const assets = [
+  "/budget/",
+  "/budget/index.html",
+  "/budget/css/budget-envelopes.css",
+  "/budget/js/budget-scripts.js",
+  "/budget/favicon.io"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(budgetingApp).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
